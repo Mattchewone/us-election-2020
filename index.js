846031e131bd6a209ee61530ee7c12384a08c5d6
Matt Chaffe <matt@mattchaffe.uk>
2020-11-06 12:52:11 +0000

    add new counted props and legend

--- index.js
+++ index.js
@@ -6,6 +6,10 @@
   const trump = candidates.find(p => p.lastName === 'Trump')
   const biden = candidates.find(p => p.lastName === 'Biden')
   
+  const eCollegeSeats = {
+    biden: 0,
+    trump: 0
+  }
   const results = stateResults.reduce((acc, state) => {
     const trimmedRes = state.results
       .filter(res => (res.candidateNpid === trump.npid || res.candidateNpid === biden.npid))
@@ -18,11 +22,22 @@
           candidate: res.candidateNpid === trump.npid ? trump : biden
         }
       })
+    
+    trimmedRes.forEach(res => {
+      // return acc += curr.votes.percentage
+      res.votes.pCounted = ((res.votes.percentage / 100) * state.expectedPercentage).toFixed(2)
+    }, 0)
+    
+    const [leader, trailing] = trimmedRes
+    // How many seats calculated on the "estimated" winner
+    eCollegeSeats.trump += leader.candidateNpid === trump.npid && leader.isWinner ? leader.delegates : 0
+    eCollegeSeats.biden += leader.candidateNpid === biden.npid && leader.isWinner ? leader.delegates : 0
 
     acc.push({
       ...state,
       results: trimmedRes,
-      diff: trimmedRes[0].votes.percentage - trimmedRes[1].votes.percentage
+      diff: leader.votes.percentage - trailing.votes.percentage,
+      diffP: `${(leader.votes.percentage - trailing.votes.percentage).toFixed(2)}%`
     })
 
     return acc
@@ -38,7 +53,8 @@
     data: {
       results,
       trump,
-      biden
+      biden,
+      eCollegeSeats
     },
     methods: {
       isDem(state) {
